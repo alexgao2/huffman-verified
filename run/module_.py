28c5f34234e59a2b0af0c800f632a82616acc51f
GURJASH <guranshanand9@gmail.com>
2026-03-11 21:08:15 -0700

    modified:   Huffman.py 	modified:   __pycache__/Huffman.cpython-312.pyc 	renamed:    trash/__main__.py -> run/__main__.py 	new file:   run/__pycache__/module_.cpython-312.pyc 	renamed:    trash/module_.py -> run/module_.py 	renamed:    trash/project -> run/project 	renamed:    trash/project-py.dtr -> run/project-py.dtr 	renamed:    trash/project.cs -> run/project.cs 	renamed:    trash/project.csproj -> run/project.csproj 	renamed:    trash/project.deps.json -> run/project.deps.json 	renamed:    trash/project.dll -> run/project.dll 	renamed:    trash/project.pdb -> run/project.pdb 	renamed:    trash/project.runtimeconfig.json -> run/project.runtimeconfig.json 	deleted:    trash/__pycache__/module_.cpython-312.pyc

--- run/module_.py
+++ run/module_.py
@@ -1,9 +1,12 @@
-# Dafny program project.dfy compiled into Python
 import sys
 from typing import Callable, Any, TypeVar, NamedTuple
 from math import floor
 from itertools import count
 
-import trash.module_ as module_
+import run.module_ as module_
 import _dafny as _dafny
+import System_ as System_
+import Huffman as Huffman
+
+# Module: module_
 
